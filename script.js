89fb1487080b16fad6ec11428f33a9247892118e
Heather Empson <hrempson@gmail.com>
2020-04-22 15:33:38 -0700

    v1

---
+++ script.js
@@ -0,0 +1,46 @@
+var timer = document.querySelector("timer");
+var question = document.querySelector("question");
+var answer = document.querySelector("answer");
+var answerButton = document.querySelector("answerbutton");
+var submitButton = document.querySelector("btn");
+var quiz = document.querySelector("question")
+//var nextQuestion = 
+
+//  secondsElapsed = 0;
+    // setTime();
+    // secondsLeft();
+
+var timer = secondsLeft
+var secondsLeft = "90"
+// countdown timer in seconds
+var secondsLeft = setInterval(function() {
+var seconds = Math.floor((distance % (1000 * secondsLeft)) / 1000);
+document.getElementById("timer").innerHTML = secondsLeft + " seconds";
+//timer.appendChild;
+
+//clear timer
+if (distance < 0) {
+    clearInterval(timer);
+    document.getElementById("timer").innerHTML = "Game Over!";
+  }
+}, 1000);
+
+
+
+
+
+document.getElementById("#question")
+//create 5 questions
+
+
+//append
+
+
+document.getElementById ("#answer")
+// create 4 answers per question
+
+
+// event listeners
+// submitButton.addEventListener("click", startTimer);
+// answerButton.addEevntListener("click", Question++)
+
