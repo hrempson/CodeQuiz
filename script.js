08044ecfe72b810bdce70aa128d5061f2a1d89c5
Heather Empson <hrempson@gmail.com>
2020-04-22 17:10:31 -0700

    question & answer array

--- script.js
+++ script.js
@@ -3,22 +3,49 @@ var question = document.querySelector("question");
 var answer = document.querySelector("answer");
 var answerButton = document.querySelector("answerbutton");
 var submitButton = document.querySelector("btn");
-var quiz = document.querySelector("question")
-//var nextQuestion = 
+var quiz = [
+    {
+    question: "What is jQuery?",
+        answers: {
+        a: "A JavaScript library",
+        b: "A text editor",
+        c: "A github repository",
+    },
+        correctAnswer: "a"
+    },
+    { question: "What is vanilla Javascript?",
+        answers: {
+        a: "A JavaScript library",
+        b: "Basic JavaScript",
+        c: "A JavaScrit Library",
+    },
+        correctAnswer: 'b'
+    },
+    { question: "Who founded JavaScript?",
+        answers: {
+        a: "Bill Gates",
+        b: "Larry Page",
+        C: "Brendan Eich",
+    }, 
+    correctAnswer: "c"
+    }];
+    
+
+
 
 //  secondsElapsed = 0;
     // setTime();
     // secondsLeft();
 
-var timer = secondsLeft
+var time = secondsLeft
 var secondsLeft = "90"
 // countdown timer in seconds
 var secondsLeft = setInterval(function() {
 var seconds = Math.floor((distance % (1000 * secondsLeft)) / 1000);
 document.getElementById("timer").innerHTML = secondsLeft + " seconds";
-//timer.appendChild;
+//timer.appendChild; ?
 
-//clear timer
+//clear timer @ Game Over
 if (distance < 0) {
     clearInterval(timer);
     document.getElementById("timer").innerHTML = "Game Over!";
@@ -39,8 +66,15 @@ document.getElementById("#question")
 document.getElementById ("#answer")
 // create 4 answers per question
 
+// on click, show next question
+function nextQuestion(event) {
+    if (answerButton === true) {
+    answerButton.addEevntListener("click", Question)
+    }
+    else ( secondsLeft - 10 ); // subtract 10s from timer
+
 
 // event listeners
 // submitButton.addEventListener("click", startTimer);
-// answerButton.addEevntListener("click", Question++)
+
 
