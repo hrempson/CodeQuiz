9d0e27516dfc2c69571b974ac012254e402e9893
Heather Empson <hrempson@gmail.com>
2020-04-24 14:48:11 -0700

    start button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var answer = document.querySelector("answer");
 var answerButton = document.querySelector("answerbutton");
 var submitButton = document.querySelector("btn");
 var currentQuestion = -1;
-var quiz = [{
+const quiz = [{
         question: "What is jQuery?",
         answers: {
             a: "A JavaScript library",
@@ -34,60 +34,74 @@ var quiz = [{
 ];
 
 
-var secondsLeft = 3;
-// countdown timer in seconds
-var countdownTimerId = setInterval(displayTime, 1000);
+// Start button
+var quizQuestions = quiz.question
+var startBtn = $("<button>Start Quiz</button>")
+startBtn.click(function () {
+    var startTimer = confirm("Go!")
+    //confirm === true, start timer
+    if (startTimer === true) {
+        startGame = displayTime;
+        // hide start.Btn 
+        startBtn.hide();
+        quizQuestions;
+        
+    }
 
-function displayTime() {
-    var seconds = secondsLeft--;
-    if (seconds > 0) {
-        document.querySelector(".timer").innerHTML = seconds + " seconds";
-    } else {
-        clearInterval(countdownTimerId);
-        document.querySelector(".timer").innerHTML = "Game Over!";
+// Timer
+    var secondsLeft = 3;
+    var countdownTimerId = setInterval(displayTime, 1000);
+
+    function displayTime() {
+        var seconds = secondsLeft--;
+        if (seconds > 0) {
+            document.querySelector(".timer").innerHTML = ":" + seconds;
+        } else {
+            clearInterval(countdownTimerId);
+            //clear timer @ Game Over////
+            document.querySelector(".timer").innerHTML = "Game Over!";
+        }
+    }
+});
+//append to html
+$(".question").append(startBtn);
+
+// 
+
+
+
+
+
+// function nextQuestion(event) {
+//     if (currentQuestion < quiz.length - 1) {
+//         currentQuestion++;
+//     } 
+//     else {
+//         alert("Game Over!")
+//     }
+
+
+// var answerBtn = currentQuestion++;
+// answerBtn.click(nextQuestion() {
+
+// }
+
+function nextQuestion() {
+    if (currentQuestion < quiz.length - 1) {
+        currentQuestion++;
+    } 
+    else {
+        alert("Game Over!");
+        // END OF QUIZ
+
     }
 }
-//timer.appendChild; ?
-
-//clear timer @ Game Over
-
-
-
-
-
-
-document.getElementById("#question")
-//create 5 questions
-
-
-//append
-
-
-document.getElementById("#answer")
-
-function nextQuestion (event) {
-    if (currentQuestion < quiz.length - 1) {
-            currentQuestion++;}
-    else {
-        // END OF QUIZ
-         console.log ('End of quiz');
-          }
-         console.log (quiz[currentQuestion]);
-          //   if (answerButton === true) {
-          //     answerButton.addEventListener ('click', Question);
-          //   } else {
-          //     secondsLeft - 10; // subtract 10s from timer
-          //   }
-        }
+$(".question").append(currentQuestion);
 
 
 // // on click, show next question
 // function nextQuestion(event) {
-//     if (answerButton === true) {
-//         answerButton.addEventListener("click", Question)
+//     if (answerBtn === true) {
+//         answerBtn.addEventListener("click", Question)
 //     } else {
 //         secondsLeft - 10; // subtract 10s from timer
-
-// }
-// event listeners
-// submitButton.addEventListener("click", startTimer)
