416a74be2f11f8e87b3fcc58c42ce0e618438e13
Heather Empson <hrempson@gmail.com>
2020-04-25 14:43:11 -0700

    answer buttons now click

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var timer = document.querySelector(".timer");
 var question = document.querySelector(".question");
 var answer = document.querySelector(".answer");
-var answer1 = document.querySelector(".answer1");
+var answer1 = document.querySelector("#answer1");
+
 var submitButton = document.querySelector(".btn");
 var score = 0;
 const quiz = [{
@@ -11,14 +12,14 @@ const quiz = [{
             "A text editor",
             "A github repository",
         ],
-        correctAnswer: "A JavaSctipt library"
+        correctAnswer: "A JavaScript library" 
     },
     {
         question: "What is vanilla Javascript?",
         answers: [
             "A JavaScript library",
             "Basic JavaScript",
-            "A JavaScrit Library",
+            "",
     ],
         correctAnswer: 'Basic JavaScript'
     },
@@ -35,15 +36,22 @@ const quiz = [{
 
 var currentQuestion = quiz[0].question;
 
-
-
-// Start button
-var quizQuestions = quiz[0].question
-
+var quizQuestions = quiz[0].question;
 var currentQuestion = quiz[0].question;
+var validateAnswer1 = quiz[0].correctAnswer
 
+
+
+// $("#answer1").click(validateAnswer) 
+//     if (validateAnswer === true);
+//     alert("Correct!")
+// console.log(validateAnswer);
+
+
+// Start Button
 var startBtn = $("<button>Start Quiz</button>")
 $(".question").html(startBtn);
+
 startBtn.click(function () {
     var startTimer = confirm("Go!")
     //confirm === true, start timer
@@ -56,15 +64,27 @@ startBtn.click(function () {
         var quizAnswers1 = quiz[0].answers[0];
         var quizAnswers2 = quiz[0].answers[1];
         var quizAnswers3 = quiz[0].answers[2];
-        // console.log(quiz[0].answers);
         var answerBtn = answer1; 
         $("#answer1").append(quizAnswers1);
         $("#answer2").append(quizAnswers2);
         $("#answer3").append(quizAnswers3);
-    }
+        // click events for answer1,2,3 buttons
+        $(".answers").on("click", function(event) 
+        { console.log($(event.target).text(),quiz[0].correctAnswer, $("#answer1").text());
+           if ($(event.target).text() === quiz[0].correctAnswer)
+               alert("correct!");
+            else {
+            secondsLeft -= 10;
+        }})
+    
+        
+
+    
+
+        
 
 // Timer
-    var secondsLeft = 3;
+    var secondsLeft = 30;
     var countdownTimerId = setInterval(displayTime, 1000);
 
     function displayTime() {
@@ -76,12 +96,14 @@ startBtn.click(function () {
             //clear timer @ Game Over////
             document.querySelector(".timer").innerHTML = "Game Over!";
         }
-    }
-});
+    }}})
+
 //append to html
 
 
-//Quiz questions begin
+//Quiz questions begin until all have been
+
+
 
 function nextQuestion() {
     if (currentQuestion < quiz.length -1) {
@@ -89,11 +111,9 @@ function nextQuestion() {
     } 
     else {
         alert("Game Over!");
+     
     }
-
-}
-
-
+};
 
 
 
